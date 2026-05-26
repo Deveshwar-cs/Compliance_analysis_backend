308be8e3262902adf66ac171d2fb6d66c5452ca0
Deveshwar-cs <goboby744@gmail.com>
2026-05-26 18:27:12 +0530

    fix: add origin correctly

--- app.js
+++ app.js
@@ -20,19 +20,14 @@ await connectQdrant();
 
 //middleware
 app.use(
-  cors(
-    {
-      origin: "http://localhost:5173", // your frontend URL
-      methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
-      credentials: true,
-    },
-    {
-      origin:
-        "https://compliance-analysis-frontend-git-main-logic-lords.vercel.app/signin", // your frontend URL
-      methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
-      credentials: true,
-    },
-  ),
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://compliance-analysis-frontend-git-main-logic-lords.vercel.app",
+    ],
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  }),
 );
 app.use(express.json());
 app.use(cookieParser());
